9ace4d3a5ae05b7dce8b3e6315311f92ead44cbb
Alejandro Pernia <alejandro.pernia@wundermanthompson.com>
2022-04-04 20:07:22 -0300

    Styling for ItemListContainer component

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,6 +1,8 @@
+import './ItemListContainer.css'
+
 const ItemListContainer = (props) => {
     return(
-        <h1>{props.greeting}</h1>
+        <h1 className="greeting-mssg">{props.greeting}</h1>
     )
 }
 

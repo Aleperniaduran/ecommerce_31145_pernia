5b233e4916be6721bc77ca5adf69252e301f75fa
Alejandro Pernia <alejandro.pernia@wundermanthompson.com>
2022-04-04 22:14:17 -0300

    ItemCount Implementation

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import NavBar from './components/NavBar/NavBar';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 
-
 function App() {
   return (
     <div className="App">
--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,9 +1,23 @@
 import './ItemListContainer.css'
+import { useState } from "react";
+import ItemCount from '../ItemCount/ItemCount';
 
 const ItemListContainer = (props) => {
+
+    const [show] = useState(true);
+    const handleOnAdd = (quantity) => {
+        console.log(`Se agregaron ${quantity} productos`);
+    };
+
     return(
-        <h1 className="greeting-mssg">{props.greeting}</h1>
-    )
-}
+        <div>
+            <h1 className="greeting-mssg">{props.greeting}</h1>
+
+            <div>
+                {show ? <ItemCount initial={5} stock={10} onAdd={handleOnAdd} /> : null}
+            </div>
+        </div>        
+    );
+};
 
 export default ItemListContainer

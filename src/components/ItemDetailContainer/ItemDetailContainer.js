e67097b1c04bf172af3c95524f2cfe4fc757b680
Alejandro Pernia <alejandro.pernia@wundermanthompson.com>
2022-04-20 22:15:26 -0300

    Implemaneting Loaders on PDP

--- src/components/ItemDetailContainer/ItemDetailContainer.js
+++ src/components/ItemDetailContainer/ItemDetailContainer.js
@@ -1,33 +1,47 @@
-import './ItemDetailContainer.css'
-import { useEffect, useState } from 'react';
-import{ getProductById } from '../../asyncmock'
-import ItemDetail from '../ItemDetail/ItemDetail'
-import { useParams} from 'react-router-dom'
+import "./ItemDetailContainer.css";
+import { useEffect, useState } from "react";
+import { getProductById } from "../../asyncmock";
+import ItemDetail from "../ItemDetail/ItemDetail";
+import { useParams } from "react-router-dom";
 
 const ItemDetailContainer = () => {
+  const [product, setProduct] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const { productId } = useParams();
 
-    const [product, setProduct] = useState([])
-    const { productId } = useParams()
+  useEffect(() => {
+    getProductById(productId)
+      .then((response) => {
+        setProduct(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
 
-    useEffect(() => {
-        getProductById(productId).then(response => {
-            setProduct(response)
-        }).catch(error => {
-            console.log(error)
-        })
+    return () => {
+      setProduct();
+    };
+  }, [productId]);
 
-        return (() => {
-            setProduct()
-        })
-
-    }, [])
-
-    return(
-        <div className='Product-Details-Page-Container'>
-            <h1>This is the PDP (Product Detail Page)</h1>
-            <ItemDetail {...product} />
-        </div>        
-    );
+  return (
+    <div className="Product-Details-Page-Container">
+          {loading ? (
+            <h2 className="">Cargando...</h2>
+          ) : product ? (
+            <div>
+              <h1>This is the PDP (Product Detail Page)</h1>
+              <ItemDetail {...product} />
+            </div>
+          ) : (
+            <h1 className="">
+              El producto no existe
+            </h1>
+          )}
+    </div>
+  );
 };
 
-export default ItemDetailContainer
+export default ItemDetailContainer;

f978692835c4919812171d2685a4e711c3313ffc
Alejandro Pernia <alejandro.pernia@wundermanthompson.com>
2022-04-27 21:22:22 -0300

    Firebase Implementation

--- src/components/ItemDetailContainer/ItemDetailContainer.js
+++ src/components/ItemDetailContainer/ItemDetailContainer.js
@@ -1,25 +1,22 @@
 import "./ItemDetailContainer.css";
 import { useEffect, useState } from "react";
-import { getProductById } from "../../asyncmock";
 import ItemDetail from "../ItemDetail/ItemDetail";
 import { useParams } from "react-router-dom";
+import { firestoreDb } from '../../services/firebase'
+import { getDoc, doc } from 'firebase/firestore'
 
 const ItemDetailContainer = ({ setCart, cart }) => {
   const [product, setProduct] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
   const { productId } = useParams();
 
   useEffect(() => {
-    getProductById(productId)
-      .then((response) => {
-        setProduct(response);
-      })
-      .catch((error) => {
-        console.log(error);
-      })
-      .finally(() => {
-        setLoading(false);
-      });
+
+    getDoc(doc(firestoreDb, 'products', productId)).then(response => {
+      console.log(response)
+      const product = { id: response.id, ...response.data()}
+      setProduct(product)
+    })
 
     return () => {
       setProduct();
